a73c12349af3d05944fa80752b8a11d3a987ec8b
Tawfiq JAFFAR <tawfiq.jaffar@epitech.eu>
2020-03-04 19:43:02 +0100

    remove unused class in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import { ThemeProvider } from '@material-ui/core';
 import React from 'react';
 import NavigationBar from './components/common/NavigationBar';
-import Router from './components/common/Router';
 import theme from './theme';
 
 function App() {
   return (
-    <div className="App">
+    <>
       <ThemeProvider theme={theme}>
         <NavigationBar />
-        <Router />
       </ThemeProvider>
-    </div>
+    </>
   );
 }
 

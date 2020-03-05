30e61e04f395ff53d4c5baefa9e38066ba78f9c9
Tawfiq Jaffar <tawfiq_mtj@hotmail.fr>
2020-03-05 03:32:54 +0100

    Carousel items and showcase title + quotes sample (#5)
    * add file
    
    * remove comment in NavigationBar
    
    * Add router
    
    * add favicon
    
    * update failing snapshots
    
    * update snapshot
    
    * add linting
    
    * correct indenting in circleci config
    
    * correct indentation again
    
    * add file
    
    * fix conflicts in circleci config
    
    * correct indenting in circleci config
    
    * correct indentation again
    
    * add file
    
    * correct indenting in circleci config
    
    * fix conflicts in circleci config
    
    * correct indentation again
    
    * add showcase and quotes

--- src/components/pages/LandingPage/LandingPage.jsx
+++ src/components/pages/LandingPage/LandingPage.jsx
@@ -1,7 +1,37 @@
+import { Container, makeStyles, Typography } from '@material-ui/core';
 import React from 'react';
+import Showcase from './Showcase';
 
-const LandingPage = () => (
-  <h1>Hello from Landing Page</h1>
-);
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: theme.palette.primary.dark,
+    textAlign: 'center',
+    padding: theme.spacing(3),
+  },
+  showcase: {
+    marginTop: theme.spacing(4),
+  },
+  quote: {
+    fontWeight: 'bold',
+    fontStyle: 'italic',
+  },
+}));
+
+const LandingPage = () => {
+  const classes = useStyles();
+  return (
+    <Container className={classes.root}>
+      <Typography variant="h2" color="textSecondary">Schola</Typography>
+      <Typography variant="h5" color="textSecondary" className={classes.quote}>
+        &ldquo;Let us remember: One book, one pen, one child and one teacher
+        can change the world.&rdquo;
+      </Typography>
+      <Typography variant="h6" color="textSecondary">
+        Join us today & manage your student life like never before
+      </Typography>
+      <Showcase className={classes.showcase} />
+    </Container>
+  );
+};
 
 export default LandingPage;

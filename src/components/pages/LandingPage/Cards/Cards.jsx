6fe8fd4a2ec62d6572690552f4c091cbfaa2b983
Tawfiq JAFFAR <tawfiq.jaffar@epitech.eu>
2020-03-09 22:11:26 +0000

    #15 add cards to describe project (4*3 columns)

---
+++ src/components/pages/LandingPage/Cards/Cards.jsx
@@ -0,0 +1,28 @@
+import { AssessmentTwoTone, ForumTwoTone, ImportContactsTwoTone } from '@material-ui/icons';
+import React from 'react';
+import CardItem from './CardItem';
+
+
+const data = [{
+  title: 'Manage your school progress',
+  description: 'Discover a new working method to boost your productivity whether you\'re a student or a teacher',
+  icon: ImportContactsTwoTone,
+}, {
+  title: 'Learn more efficiently',
+  description: 'We garanty at least +2 points on your year average if you choose our solution for your studies',
+  icon: AssessmentTwoTone,
+}, {
+  title: 'Exchange with teachers',
+  description: 'Get help from those you need at the right moment. Chat with your teachers efficiently',
+  icon: ForumTwoTone,
+}];
+
+const Cards = () => (
+  <>
+    {data.map((el) => (
+      <CardItem icon={el.icon} title={el.title} description={el.description} />
+    ))}
+  </>
+);
+
+export default Cards;

5ddc020d0ecd330e3628062f4109a677808bf82f
Thomas Daullé <thomas.daulle@epitech.eu>
2020-05-13 21:23:14 +0200

    [MODIFY] controllers folder

--- packages/backend/src/models/userSchema.js
+++ packages/backend/src/models/userSchema.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
+const passwordHash = require('password-hash');
+const jwt = require('jwt-simple');
+const config = require('../../config/config');
 
-const schema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true,
@@ -33,4 +36,13 @@ const schema = new mongoose.Schema({
   },
 });
 
-module.exports = mongoose.model('User', schema);
+userSchema.methods = {
+  authenticate: function(password) {
+    return passwordHash.verify(password, this.password);
+  },
+  getToken: function() {
+    return jwt.encode(this, config.secret);
+  }
+}
+
+module.exports = mongoose.model('User', userSchema);

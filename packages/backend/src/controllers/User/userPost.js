362776d510721a2238a9db9916646951d35c5365
Tawfiq JAFFAR <tawfiq@6degres.com>
2020-05-15 21:06:34 +0200

    [BACK] #31: move routes methods to controllers and check access rights before chaning user's role

--- packages/backend/src/controllers/User/userPost.js
+++ packages/backend/src/controllers/User/userPost.js
@@ -1,1 +1,65 @@
 // request creating some docs in db about user
+
+const User = require('../../models/user');
+const responseBody = require('../../routes/responseBody');
+const { hashPassword } = require('../../encryption/hash');
+
+const postCreateUser = (req, res) => {
+  const {
+    username,
+    password,
+    firstname,
+    lastname,
+    email,
+    dateofbirth,
+    role,
+  } = req.body;
+
+  const createUser = async () => {
+    const hashed = await hashPassword(password);
+    const newUser = new User({
+      userName: username,
+      password: hashed,
+      firstName: firstname,
+      lastName: lastname,
+      email,
+      dateOfBirth: dateofbirth,
+      role,
+    });
+    newUser.save((err, user) => {
+      if (err) {
+        return res
+          .status(responseBody.responseCode.INTSERVERR)
+          .send(
+            responseBody.buildResponseBody(
+              err,
+              responseBody.responseCode.INTSERVERR
+            )
+          );
+      }
+      return res
+        .status(responseBody.responseCode.SUCCESS)
+        .send(responseBody.buildResponseBody(user));
+    });
+  };
+  User.find({ $or: [{ userName: username }, { email }] }, async (err, user) => {
+    if (err || user.length > 0) {
+      const conflicted = user[0].userName === username ? username : email;
+      return res.status(responseBody.responseCode.CONFLICT).send(
+        responseBody.buildResponseBody(
+          {
+            error: `${conflicted} is already in use`,
+          },
+          responseBody.responseCode.CONFLICT
+        )
+      );
+    }
+    const newUser = await createUser();
+
+    return newUser;
+  });
+};
+
+module.exports = {
+  postCreateUser,
+};

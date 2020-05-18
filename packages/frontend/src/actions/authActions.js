b95b428268b44e5617dc1e84036f760274dc205c
Tawfiq JAFFAR <tawfiq.jaffar@epitech.eu>
2020-05-18 20:35:01 +0200

    [FRONT] #30: corrected error 400 on login page

--- packages/frontend/src/actions/authActions.js
+++ packages/frontend/src/actions/authActions.js
@@ -1,35 +1,40 @@
 import axios from 'axios';
-import setAuthorizationToken from '../utils/setAuthorizationToken';
 import jwtDecode from 'jwt-decode';
+import setAuthorizationToken from '../utils/setAuthorizationToken';
 import { SET_CURRENT_USER } from './types';
 
 export function setCurrentUser(user) {
   return {
     type: SET_CURRENT_USER,
-    user
+    user,
   };
 }
 
 export function logout() {
-  return dispatch => {
+  return (dispatch) => {
     localStorage.removeItem('jwtToken');
     setAuthorizationToken(false);
     dispatch(setCurrentUser({}));
-  }
+  };
 }
 
 export function login(data) {
-  const head = { headers: {
-    "Access-Control-Allow-Origin": "*/*",
-    "Content-Type": "application/x-www-form-urlencoded",
-    }
-  }
-  return dispatch => {
-    return axios.post('http://localhost:8080/api/auth/login', {email: data.email, password: data.password}, head).then(res => {
-      const token = res.data.token;
-      localStorage.setItem('jwtToken', token);
-      setAuthorizationToken(token);
-      dispatch(setCurrentUser(jwtDecode(token)));
-    });
-  }
+  const head = {
+    headers: {
+      'Access-Control-Allow-Origin': '*/*',
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+  };
+  const { email, password } = data;
+
+  return (dispatch) => {
+    return axios
+      .post('http://localhost:8080/api/auth/login', { email, password })
+      .then((res) => {
+        const { token } = res.data;
+        localStorage.setItem('jwtToken', token);
+        setAuthorizationToken(token);
+        dispatch(setCurrentUser(jwtDecode(token)));
+      });
+  };
 }

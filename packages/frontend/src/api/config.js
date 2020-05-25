4417401b44a2bb91bfd1e4e3dae1fcb3e7d44c92
Tawfiq JAFFAR <tawfiq.jaffar@epitech.eu>
2020-05-25 23:11:17 +0200

    add new url to prod server

--- packages/frontend/src/api/config.js
+++ packages/frontend/src/api/config.js
@@ -2,7 +2,6 @@ import 'dotenv';
 import axios from 'axios';
 
 const serverUrl = process.env.SERVER_URI || 'http://localhost:8080/api';
-
 const axiosInstance = axios.create({
   baseURL: serverUrl,
   timeout: 10000,

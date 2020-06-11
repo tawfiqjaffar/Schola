f61070e9694d7e2b05bae8fe68a97f40465b54a9
kevin.hu@epitech.eu <kevin.hu@epitech.eu@epitech.eu>
2020-06-11 16:10:01 +0200

    [FRONT] #33 changing template year to 2020 to be looked like nowaday scheduler

--- packages/frontend/src/components/pages/Calendar/Calendar.jsx
+++ packages/frontend/src/components/pages/Calendar/Calendar.jsx
@@ -28,62 +28,62 @@ const appointments = [
   {
     id: 0,
     title: 'Watercolor Landscape',
-    startDate: new Date(2018, 6, 23, 9, 30),
-    endDate: new Date(2018, 6, 23, 11, 30),
+    startDate: new Date(2020, 6, 23, 9, 30),
+    endDate: new Date(2020, 6, 23, 11, 30),
     ownerId: 1,
   }, {
     id: 1,
     title: 'Monthly Planning',
-    startDate: new Date(2018, 5, 28, 9, 30),
-    endDate: new Date(2018, 5, 28, 11, 30),
+    startDate: new Date(2020, 5, 28, 9, 30),
+    endDate: new Date(2020, 5, 28, 11, 30),
     ownerId: 1,
   }, {
     id: 2,
     title: 'Recruiting students',
-    startDate: new Date(2018, 6, 9, 12, 0),
-    endDate: new Date(2018, 6, 9, 13, 0),
+    startDate: new Date(2020, 6, 9, 12, 0),
+    endDate: new Date(2020, 6, 9, 13, 0),
     ownerId: 2,
   }, {
     id: 3,
     title: 'Oil Painting',
-    startDate: new Date(2018, 6, 18, 14, 30),
-    endDate: new Date(2018, 6, 18, 15, 30),
+    startDate: new Date(2020, 6, 18, 14, 30),
+    endDate: new Date(2020, 6, 18, 15, 30),
     ownerId: 2,
   }, {
     id: 4,
     title: 'Open Day',
-    startDate: new Date(2018, 6, 20, 12, 0),
-    endDate: new Date(2018, 6, 20, 13, 35),
+    startDate: new Date(2020, 6, 20, 12, 0),
+    endDate: new Date(2020, 6, 20, 13, 35),
     ownerId: 6,
   }, {
     id: 5,
     title: 'Watercolor Landscape',
-    startDate: new Date(2018, 6, 6, 13, 0),
-    endDate: new Date(2018, 6, 6, 14, 0),
-    rRule: 'FREQ=WEEKLY;BYDAY=FR;UNTIL=20180816',
-    exDate: '20180713T100000Z,20180727T100000Z',
+    startDate: new Date(2020, 6, 6, 13, 0),
+    endDate: new Date(2020, 6, 6, 14, 0),
+    rRule: 'FREQ=WEEKLY;BYDAY=FR;UNTIL=20200816',
+    exDate: '20200713T100000Z,20200727T100000Z',
     ownerId: 2,
   }, {
     id: 6,
     title: 'Meeting of Instructors',
-    startDate: new Date(2018, 5, 28, 12, 0),
-    endDate: new Date(2018, 5, 28, 12, 30),
-    rRule: 'FREQ=WEEKLY;BYDAY=TH;UNTIL=20180727',
-    exDate: '20180705T090000Z,20180719T090000Z',
+    startDate: new Date(2020, 5, 28, 12, 0),
+    endDate: new Date(2020, 5, 28, 12, 30),
+    rRule: 'FREQ=WEEKLY;BYDAY=TH;UNTIL=20200727',
+    exDate: '20200705T090000Z,20200719T090000Z',
     ownerId: 5,
   }, {
     id: 7,
     title: 'Oil Painting for Beginners',
-    startDate: new Date(2018, 6, 3, 11, 0),
-    endDate: new Date(2018, 6, 3, 12, 0),
-    rRule: 'FREQ=WEEKLY;BYDAY=TU;UNTIL=20180801',
-    exDate: '20180710T080000Z,20180724T080000Z',
+    startDate: new Date(2020, 6, 3, 11, 0),
+    endDate: new Date(2020, 6, 3, 12, 0),
+    rRule: 'FREQ=WEEKLY;BYDAY=TU;UNTIL=20200801',
+    exDate: '20200710T080000Z,20200724T080000Z',
     ownerId: 3,
   }, {
     id: 8,
     title: 'Watercolor Workshop',
-    startDate: new Date(2018, 6, 9, 11, 0),
-    endDate: new Date(2018, 6, 9, 12, 0),
+    startDate: new Date(2020, 6, 9, 11, 0),
+    endDate: new Date(2020, 6, 9, 12, 0),
     ownerId: 3,
   },
 ];
@@ -336,7 +336,7 @@ export default class Demo extends React.PureComponent {
             onCommitChanges={this.commitChanges}
           />
           <ViewState
-            defaultCurrentDate="2018-07-17"
+            defaultCurrentDate="2020-07-17"
           />
 
           <MonthView

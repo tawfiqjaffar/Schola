70d9985b470b520f9a591255f0e23cf10f2488c1
Kevin Hu <kevin.hu@epitech.eu>
2020-06-12 17:18:19 +0200

    [FRONT] #33 - little design fix

--- packages/frontend/src/components/pages/Calendar/Calendar.jsx
+++ packages/frontend/src/components/pages/Calendar/Calendar.jsx
@@ -1,7 +1,11 @@
 import * as React from 'react';
 import Paper from '@material-ui/core/Paper';
 import TableCell from '@material-ui/core/TableCell';
-import { darken, fade, lighten } from '@material-ui/core/styles/colorManipulator';
+import {
+  darken,
+  fade,
+  lighten,
+} from '@material-ui/core/styles/colorManipulator';
 import Typography from '@material-ui/core/Typography';
 import { ViewState, EditingState } from '@devexpress/dx-react-scheduler';
 import classNames from 'clsx';
@@ -31,31 +35,36 @@ const appointments = [
     startDate: new Date(2020, 6, 23, 9, 30),
     endDate: new Date(2020, 6, 23, 11, 30),
     ownerId: 1,
-  }, {
+  },
+  {
     id: 1,
     title: 'Monthly Planning',
     startDate: new Date(2020, 5, 28, 9, 30),
     endDate: new Date(2020, 5, 28, 11, 30),
     ownerId: 1,
-  }, {
+  },
+  {
     id: 2,
     title: 'Recruiting students',
     startDate: new Date(2020, 6, 9, 12, 0),
     endDate: new Date(2020, 6, 9, 13, 0),
     ownerId: 2,
-  }, {
+  },
+  {
     id: 3,
     title: 'Oil Painting',
     startDate: new Date(2020, 6, 18, 14, 30),
     endDate: new Date(2020, 6, 18, 15, 30),
     ownerId: 2,
-  }, {
+  },
+  {
     id: 4,
     title: 'Open Day',
     startDate: new Date(2020, 6, 20, 12, 0),
     endDate: new Date(2020, 6, 20, 13, 35),
     ownerId: 6,
-  }, {
+  },
+  {
     id: 5,
     title: 'Watercolor Landscape',
     startDate: new Date(2020, 6, 6, 13, 0),
@@ -63,7 +72,8 @@ const appointments = [
     rRule: 'FREQ=WEEKLY;BYDAY=FR;UNTIL=20200816',
     exDate: '20200713T100000Z,20200727T100000Z',
     ownerId: 2,
-  }, {
+  },
+  {
     id: 6,
     title: 'Meeting of Instructors',
     startDate: new Date(2020, 5, 28, 12, 0),
@@ -71,7 +81,8 @@ const appointments = [
     rRule: 'FREQ=WEEKLY;BYDAY=TH;UNTIL=20200727',
     exDate: '20200705T090000Z,20200719T090000Z',
     ownerId: 5,
-  }, {
+  },
+  {
     id: 7,
     title: 'Oil Painting for Beginners',
     startDate: new Date(2020, 6, 3, 11, 0),
@@ -79,7 +90,8 @@ const appointments = [
     rRule: 'FREQ=WEEKLY;BYDAY=TU;UNTIL=20200801',
     exDate: '20200710T080000Z,20200724T080000Z',
     ownerId: 3,
-  }, {
+  },
+  {
     id: 8,
     title: 'Watercolor Workshop',
     startDate: new Date(2020, 6, 9, 11, 0),
@@ -88,23 +100,29 @@ const appointments = [
   },
 ];
 
-const resources = [{
-  fieldName: 'ownerId',
-  title: 'Owners',
-  instances: owners,
-}];
+const resources = [
+  {
+    fieldName: 'ownerId',
+    title: 'Owners',
+    instances: owners,
+  },
+];
 
-const getBorder = theme => (`1px solid ${
-  theme.palette.type === 'light'
-    ? lighten(fade(theme.palette.divider, 1), 0.88)
-    : darken(fade(theme.palette.divider, 1), 0.68)
-}`);
+const getBorder = (theme) =>
+  `1px solid ${
+    theme.palette.type === 'light'
+      ? lighten(fade(theme.palette.divider, 1), 0.88)
+      : darken(fade(theme.palette.divider, 1), 0.68)
+  }`;
 
-const DayScaleCell = props => (
-  <MonthView.DayScaleCell {...props} style={{ textAlign: 'center', fontWeight: 'bold' }} />
+const DayScaleCell = (props) => (
+  <MonthView.DayScaleCell
+  {...props}
+    style={{ textAlign: 'center', fontWeight: 'bold' }}
+  />
 );
 
-const styles = theme => ({
+const styles = (theme) => ({
   cell: {
     color: '#78909C!important',
     position: 'relative',
@@ -238,60 +256,70 @@ const WeatherIcon = ({ classes, id }) => {
 };
 
 // #FOLD_BLOCK
-const CellBase = React.memo(({
-  classes,
-  startDate,
-  formatDate,
-  otherMonth,
-  // #FOLD_BLOCK
-}) => {
-  const iconId = Math.abs(Math.floor(Math.sin(startDate.getDate()) * 10) % 3);
-  const isFirstMonthDay = startDate.getDate() === 1;
-  const formatOptions = isFirstMonthDay
-    ? { day: 'numeric', month: 'long' }
-    : { day: 'numeric' };
-  return (
-    <TableCell
-      tabIndex={0}
-      className={classNames({
-        [classes.cell]: true,
-        [classes.rainBack]: iconId === 0,
-        [classes.sunBack]: iconId === 1,
-        [classes.cloudBack]: iconId === 2,
-        [classes.opacity]: otherMonth,
-      })}
-    >
-      <div className={classes.content}>
-        <WeatherIcon classes={classes} id={iconId} />
-      </div>
-      <div className={classes.text}>
-        {formatDate(startDate, formatOptions)}
-      </div>
-    </TableCell>
-  );
-});
+const CellBase = React.memo(
+  ({
+    classes,
+    startDate,
+    formatDate,
+    otherMonth,
+    // #FOLD_BLOCK
+  }) => {
+    const iconId = Math.abs(Math.floor(Math.sin(startDate.getDate()) * 10) % 3);
+    const isFirstMonthDay = startDate.getDate() === 1;
+    const formatOptions = isFirstMonthDay
+      ? { day: 'numeric', month: 'long' }
+      : { day: 'numeric' };
+    return (
+      <TableCell
+        tabIndex={0}
+        className={classNames({
+          [classes.cell]: true,
+          [classes.rainBack]: iconId === 0,
+          [classes.sunBack]: iconId === 1,
+          [classes.cloudBack]: iconId === 2,
+          [classes.opacity]: otherMonth,
+        })}
+      >
+        <div className={classes.content}>
+          <WeatherIcon classes={classes} id={iconId} />
+        </div>
+        <div className={classes.text}>
+          {formatDate(startDate, formatOptions)}
+        </div>
+      </TableCell>
+    );
+  }
+);
 
 const TimeTableCell = withStyles(styles, { name: 'Cell' })(CellBase);
 
-const Appointment = withStyles(styles, { name: 'Appointment' })(({ classes, ...restProps }) => (
-  <Appointments.Appointment
+const Appointment = withStyles(styles, {
+  name: 'Appointment',
+})(({ classes, ...restProps }) => (
+  <Appointments.Appointment {...restProps} className={classes.appointment} />
+));
+
+const AppointmentContent = withStyles(styles, {
+  name: 'AppointmentContent',
+})(({ classes, ...restProps }) => (
+  <Appointments.AppointmentContent
     {...restProps}
-    className={classes.appointment}
+    className={classes.apptContent}
   />
 ));
 
-const AppointmentContent = withStyles(styles, { name: 'AppointmentContent' })(({ classes, ...restProps }) => (
-  <Appointments.AppointmentContent {...restProps} className={classes.apptContent} />
-));
-
-const FlexibleSpace = withStyles(styles, { name: 'ToolbarRoot' })(({ classes, ...restProps }) => (
-  <Toolbar.FlexibleSpace {...restProps} className={classes.flexibleSpace}>
-    <div className={classes.flexContainer}>
-      <ColorLens fontSize="large" htmlColor="#FF7043" />
-      <Typography variant="h5" style={{ marginLeft: '10px' }}>Art School</Typography>
-    </div>
-  </Toolbar.FlexibleSpace>
-));
+const FlexibleSpace = withStyles(styles, { name: 'ToolbarRoot' })(
+  ({ classes, ...restProps }) => (
+    <Toolbar.FlexibleSpace {...restProps} className={classes.flexibleSpace}>
+      <div className={classes.flexContainer}>
+        <ColorLens fontSize="large" htmlColor="#FF7043" />
+        <Typography variant="h5" style={{ marginLeft: '10px' }}>
+          Schedule
+        </Typography>
+      </div>
+    </Toolbar.FlexibleSpace>
+  )
+);
 
 export default class Demo extends React.PureComponent {
   // #FOLD_BLOCK
@@ -310,15 +338,19 @@ export default class Demo extends React.PureComponent {
     this.setState((state) => {
       let { data } = state;
       if (added) {
-        const startingAddedId = data.length > 0 ? data[data.length - 1].id + 1 : 0;
+        const startingAddedId =
+          data.length > 0 ? data[data.length - 1].id + 1 : 0;
         data = [...data, { id: startingAddedId, ...added }];
       }
       if (changed) {
-        data = data.map(appointment => (
-          changed[appointment.id] ? { ...appointment, ...changed[appointment.id] } : appointment));
+        data = data.map((appointment) =>
+          changed[appointment.id]
+            ? { ...appointment, ...changed[appointment.id] }
+            : appointment
+        );
       }
       if (deleted !== undefined) {
-        data = data.filter(appointment => appointment.id !== deleted);
+        data = data.filter((appointment) => appointment.id !== deleted);
       }
       return { data };
     });
@@ -329,15 +361,9 @@ export default class Demo extends React.PureComponent {
 
     return (
       <Paper>
-        <Scheduler
-          data={data}
-        >
-          <EditingState
-            onCommitChanges={this.commitChanges}
-          />
-          <ViewState
-            defaultCurrentDate="2020-07-17"
-          />
+        <Scheduler data={data}>
+          <EditingState onCommitChanges={this.commitChanges} />
+          <ViewState defaultCurrentDate="2020-07-17" />
 
           <MonthView
             timeTableCellComponent={TimeTableCell}
@@ -348,21 +374,13 @@ export default class Demo extends React.PureComponent {
             appointmentComponent={Appointment}
             appointmentContentComponent={AppointmentContent}
           />
-          <Resources
-            data={resources}
-          />
+          <Resources data={resources} />
 
-          <Toolbar
-            flexibleSpaceComponent={FlexibleSpace}
-          />
+          <Toolbar flexibleSpaceComponent={FlexibleSpace} />
           <DateNavigator />
 
           <EditRecurrenceMenu />
-          <AppointmentTooltip
-            showCloseButton
-            showDeleteButton
-            showOpenButton
-          />
+          <AppointmentTooltip showCloseButton showDeleteButton showOpenButton />
           <AppointmentForm />
           <DragDropProvider />
         </Scheduler>

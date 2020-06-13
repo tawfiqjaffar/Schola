b81fe2bad64f67b516521286f70f25171f86b765
Alan Cataldo <alan.cataldo@epitech.eu>
2020-06-13 16:40:42 +0200

    #21 [FRONT] QA: Linting on QA folder again

--- QA/AutomatedTesting/tests/Login.js
+++ QA/AutomatedTesting/tests/Login.js
@@ -1,33 +1,33 @@
 module.exports = {
-    '@tags': ['login', 'schola'],
-    'Login success': function (browser) {
+    "@tags": ["login", "schola"],
+    "Login success": function (browser) {
         var settings = browser.globals,
             identifiers = browser.globals.identifiers;
 
         browser
-            .url('http://localhost:3000/Login')
-            .waitForElementVisible('body')
-            .assert.titleContains('Schola')
-            .assert.visible('input[type=text]')
-            .assert.visible('input[type=password]')
+            .url("http://localhost:3000/Login")
+            .waitForElementVisible("body")
+            .assert.titleContains("Schola")
+            .assert.visible("input[type=text]")
+            .assert.visible("input[type=password]")
             .setValue('input[type="text"]', identifiers.good_username)
             .setValue('input[type="password"]', identifiers.good_password)
             .pause(1000)
             .end();
     },
-    'Login failure': function (browser) {
+    "Login failure": function (browser) {
         var settings = browser.globals,
             identifiers = browser.globals.identifiers;
 
         browser
-            .url('http://localhost:3000/Login')
-            .waitForElementVisible('body')
-            .assert.titleContains('Schola')
-            .assert.visible('input[type=text]')
-            .assert.visible('input[type=password]')
+            .url("http://localhost:3000/Login")
+            .waitForElementVisible("body")
+            .assert.titleContains("Schola")
+            .assert.visible("input[type=text]")
+            .assert.visible("input[type=password]")
             .setValue('input[type="text"]', identifiers.bad_username)
             .setValue('input[type="password"]', identifiers.bad_password)
             .pause(1000)
             .end();
-    }
-}
+    },
+};

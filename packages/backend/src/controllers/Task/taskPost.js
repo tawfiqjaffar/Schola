e181a860fd9778f82202869ec719a14f1e73f55e
Thomas Daullé <thomas.daulle@epitech.eu>
2020-06-14 22:47:51 +0200

    [ADD] Request create for schedule

--- packages/backend/src/controllers/Task/taskPost.js
+++ packages/backend/src/controllers/Task/taskPost.js
@@ -2,8 +2,15 @@ const Task = require('../../models/task');
 const responseBody = require('../../routes/responseBody');
 
 const createTask = (req, res) => {
-    Task.find({}, (err, data) => {
+    delete req.body._id;
+
+    const task = new Task({
+        ...req.body
+    })
+
+    task.save((err, data) => {
         if (err) {
+            console.log(err);
             return res.status(responseBody.responseCode.INTSERVERR)
             .send(
                 responseBody.buildResponseBody(error, responseBody.responseCode.INTSERVERR)

a72c61c46773306dda1fb6c111077fc57fafd83d
Thomas Daullé <thomas.daulle@epitech.eu>
2020-06-14 13:27:27 +0200

    [ADD] task delete request

--- packages/backend/src/controllers/Task/taskDelete.js
+++ packages/backend/src/controllers/Task/taskDelete.js
@@ -2,19 +2,30 @@ const Task = require('../../models/task');
 const responseBody = require('../../routes/responseBody');
 
 const deleteTask = (req, res) => {
-    Task.find({}, (err, data) => {
-        if (err) {
-            return res.status(responseBody.responseCode.INTSERVERR)
-            .send(
-                responseBody.buildResponseBody(error, responseBody.responseCode.INTSERVERR)
-            );
-        } else {
-            return res.status(responseBody.responseCode.SUCCESS)
-            .send(
-                responseBody.buildResponseBody(data, responseBody.responseCode.SUCCESS)
-            );
-        }
-    })
+    if (req.user.role === 'teacher' || req.user.role === 'viesco' || req.user.role == 'admin') {
+        Task.deleteOne({_id: req.body.id}, (err, data) => {
+            if (err) {
+                console.log(err);
+                return res.status(responseBody.responseCode.INTSERVERR)
+                .send(
+                    responseBody.buildResponseBody(error, responseBody.responseCode.INTSERVERR)
+                );
+            } else {
+                return res.status(responseBody.responseCode.SUCCESS)
+                .send(
+                    responseBody.buildResponseBody(data, responseBody.responseCode.SUCCESS)
+                );
+            }
+        })
+    } else {
+        return res.status(responseBody.responseCode.FORBID)
+        .send(
+            responseBody.buildResponseBody(
+                'You do not have the access right to perform such a modification',
+                responseBody.responseCode.FORBID
+            )
+        );
+    }
 }
 
 module.exports = {

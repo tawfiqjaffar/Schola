de0fc7735a507c035f12e5db2c60d8f0db0755d6
philippe.roudaut@epitech.eu <philippe.roudaut@epitech.eu@epitech.eu>
2020-08-25 20:51:53 +0200

    [FRONT] created a fake classes json with student name and statut, display card and change statut and color onClick #95

--- packages/frontend/src/components/pages/RollCall/RollCall.jsx
+++ packages/frontend/src/components/pages/RollCall/RollCall.jsx
@@ -1,14 +1,7 @@
 import React, { Component } from 'react';
-import {
-  TableContainer,
-  Table,
-  Paper,
-  TableHead,
-  TableRow,
-  TableCell,
-  TableBody,
-} from '@material-ui/core';
-import StatusRow from './StudentRow';
+import { Switch } from 'react-router-dom';
+import StatutRow from './StudentRow';
+import './RollCall.css';
 
 function createData(name, statut) {
   return { name, statut };
@@ -19,31 +12,54 @@ const student = [
   createData('B', 'présent'),
   createData('C', 'absent'),
   createData('D', 'présent'),
+  createData('E', 'présent'),
+  createData('F', 'présent'),
+  createData('G', 'absent'),
+  createData('H', 'présent'),
+  createData('I', 'présent'),
+  createData('J', 'présent'),
+  createData('K', 'absent'),
+  createData('L', 'présent'),
+  createData('M', 'présent'),
+  createData('N', 'présent'),
+  createData('O', 'absent'),
+  createData('P', 'présent'),
+  createData('Q', 'présent'),
+  createData('R', 'présent'),
+  createData('S', 'absent'),
+  createData('T', 'présent'),
+  createData('U', 'présent'),
+  createData('V', 'présent'),
+  createData('W', 'absent'),
+  createData('X', 'présent'),
+  createData('Y', 'présent'),
+  createData('Z', 'présent'),
 ];
 
 class RollCallPage extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      switchCheck: false,
+    };
   }
 
   render() {
+    const { switchCheck } = this.state;
     return (
-      <TableContainer component={Paper}>
-        <Table size="small" aria-label="a dense table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Etudiants</TableCell>
-              <TableCell>Statut</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {student.map((row) => (
-              <StatusRow student={row.name} status={row.statut} />
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+      <div>
+        <Switch
+          checked={switchCheck}
+          // onChange={handleChange}
+          name="Appel Basic"
+          inputProps={{ 'aria-label': 'secondary checkbox' }}
+        />
+        <div className="card-list">
+          {student.map((row) => (
+            <StatutRow student={row.name} statut={row.statut} />
+          ))}
+        </div>
+      </div>
     );
   }
 }
--- packages/frontend/src/components/pages/RollCall/StudentRow.jsx
+++ packages/frontend/src/components/pages/RollCall/StudentRow.jsx
@@ -1,20 +1,39 @@
-import React from 'react';
-import { TableCell, TableRow } from '@material-ui/core';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-const StatusRow = (props) => {
-  const { student, status } = props;
+import './RollCall.css';
+
+function changeStatut(color, setColor) {
+  console.log('COLOR CHECK');
+  if (color === '#77dd77') setColor('#FDFD98');
+  if (color === '#FDFD98') setColor('#FF9E0F');
+  if (color === '#FF9E0F') setColor('#FE6B64');
+  if (color === '#FE6B64') setColor('#77dd77');
+}
+
+const StatutRow = (props) => {
+  const { student, statut } = props;
+  const [color, setColor] = useState('#77dd77');
+  // const { name } = student;
   return (
-    <TableRow>
-      <TableCell>{student}</TableCell>
-      <TableCell>{status}</TableCell>
-    </TableRow>
+    <div
+      className="card"
+      style={{ backgroundColor: color }}
+      onClick={() => {
+        console.log(color);
+        changeStatut(color, setColor);
+      }}
+    >
+      <div className="card_title title-white">
+        <p>{student}</p>
+      </div>
+    </div>
   );
 };
 
-StatusRow.propTypes = {
+StatutRow.propTypes = {
   student: PropTypes.string.isRequired,
-  status: PropTypes.string.isRequired,
+  statut: PropTypes.string.isRequired,
 };
 
-export default StatusRow;
+export default StatutRow;

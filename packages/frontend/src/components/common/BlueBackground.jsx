5a2da09ed76610480dd58bfb6cf5e13f29e10f9f
philippe.roudaut@epitech.eu <philippe.roudaut@epitech.eu@epitech.eu>
2020-10-18 18:21:24 +0200

    [FRONT] Blue Background component to convert mockup #95

---
+++ packages/frontend/src/components/common/BlueBackground.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const BlueBackground = (props) => {
+  const { width, height, backRota, frontRota } = props;
+  const theme = {
+    back: {
+      width: `${width}px`,
+      height: `${height}px`,
+      backgroundColor: '#9ed1cc',
+      transform: `rotate(${backRota}deg)`,
+    },
+    front: {
+      width: `${width}px`,
+      height: `${height}px`,
+      backgroundColor: '#70c6c7',
+      transform: `rotate(${frontRota}deg)`,
+    },
+  };
+
+  return (
+    <div style={theme.back}>
+      <div style={theme.front}>
+      </div>
+    </div>
+  );
+};
+
+export default BlueBackground;
+
+BlueBackground.propTypes = {
+  width: PropTypes.string,
+  height: PropTypes.string,
+  backRota: PropTypes.string,
+  frontRota: PropTypes.string,
+};
+
+BlueBackground.defaultProps = {
+  width: '200',
+  height: '280',
+  backRota: '3',
+  frontRota: '-10',
+};

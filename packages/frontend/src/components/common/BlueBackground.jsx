8a96f7646424a5c47c2920d60d070c32c63b8a3b
Maciintosh <philippe_roudaut@yahoo.fr>
2020-10-21 01:45:15 +0200

    [FRONT] Blue Background component to convert mockup #95 (#110)
    * [FRONT] Blue Background component to convert mockup #95
    
    * [FRONT] fix linting problem #95
    
    * [FRONT] Component now encapsule children component #95
    
    * [FRONT] fix color, still need to fix rotation of children component #95
    
    * [FRONT] Rotation now reset for child #95
    
    * [FRONT] fix linting #95
    
    Co-authored-by: philippe.roudaut@epitech.eu <philippe.roudaut@epitech.eu@epitech.eu>

---
+++ packages/frontend/src/components/common/BlueBackground.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const BlueBackground = (props) => {
+  const { width, height, backRota, frontRota, children } = props;
+  const resetRota = (Number(frontRota) + Number(backRota)) * -1;
+  const theme = {
+    back: {
+      width: `${width}px`,
+      height: `${height}px`,
+      backgroundColor: '#9ed1cc',
+      transform: `rotate(${backRota}deg)`,
+    },
+    front: {
+      width: `${width}px`,
+      height: `${height}px`,
+      backgroundColor: '#70c6c7',
+      transform: `rotate(${frontRota}deg)`,
+    },
+    reset: {
+      transform: `rotate(${resetRota}deg)`,
+    }
+  };
+  return (
+    <div style={theme.back}>
+      <div style={theme.front}>
+        <div style={theme.reset}>
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BlueBackground;
+
+BlueBackground.propTypes = {
+  children: PropTypes.node.isRequired,
+  width: PropTypes.string,
+  height: PropTypes.string,
+  backRota: PropTypes.string,
+  frontRota: PropTypes.string,
+};
+
+BlueBackground.defaultProps = {
+  width: '200',
+  height: '280',
+  backRota: '3',
+  frontRota: '-10',
+};

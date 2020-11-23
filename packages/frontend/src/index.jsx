7aa1d856d06c616952b4c501325e079e318ecece
Maciintosh <philippe_roudaut@yahoo.fr>
2020-11-23 17:23:12 +0100

    Added Redux Architecture ( from Taw's BackOffice ), change Token stor… (#125)
    * Added Redux Architecture ( from Taw's BackOffice ), change Token storage from SessionStorage to Cookies, Recoded all Login function to make it work with Redux Architecture
    
    * Remake snapshot test
    
    * removed old Login page
    
    * tjaf: correctly added new snapshots
    
    Co-authored-by: philippe.roudaut@epitech.eu <philippe.roudaut@epitech.eu@epitech.eu>
    Co-authored-by: Tawfiq JAFFAR <tawfiq.jaffar@epitech.eu>

--- packages/frontend/src/index.jsx
+++ packages/frontend/src/index.jsx
@@ -1,6 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import Cookies from 'js-cookie';
+import allReducers from './redux/reducers';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+  allReducers,
+  { isLogged: Cookies.get('accessToken') !== undefined },
+  // eslint-disable-next-line no-underscore-dangle
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
+  document.getElementById('root')
+);

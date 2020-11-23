79f979e068d4d8936a1c207bee28295a3dd811a7
kevin.hu@epitech.eu <kevin.hu@epitech.eu@epitech.eu>
2020-11-23 21:27:18 +0100

    adding new schedule starting linking

--- packages/frontend/src/components/common/Router.jsx
+++ packages/frontend/src/components/common/Router.jsx
@@ -6,6 +6,8 @@ import Login from '../pages/Login';
 import Home from '../pages/HomePage/HomePage';
 import NavigationBar from './NavigationBar';
 import Schedule from '../pages/Schedule/index';
+// import Join from '../pages/Chatbox/Join/Join';
+// import Chatbox from '../pages/Chatbox/Chat/Chat';
 
 const redirectToLogin = (Component) => {
   if (!sessionStorage.getItem('token')) return <Login />;
@@ -20,6 +22,7 @@ const Router = () => (
       <Route path="/login" component={Login} />
       <Route path="/home" render={() => redirectToLogin(Home)} />
       <Route path="/schedule" component={Schedule}/>
+      {/* <Route path="/join" component={Join}/> */}
       <Route component={PageNotFound} />
     </Switch>
   </BrowserRouter>
--- packages/frontend/src/components/pages/Schedule/index.jsx
+++ packages/frontend/src/components/pages/Schedule/index.jsx
@@ -1,54 +1,34 @@
-import React from 'react'
+import React from 'react';
+import Paper from '@material-ui/core/Paper';
+import { ViewState } from '@devexpress/dx-react-scheduler';
+import {
+  Scheduler,
+  DayView,
+  Appointments,
+} from '@devexpress/dx-react-scheduler-material-ui';
+
+const currentDate = '2018-11-01';
+const schedulerData = [
+  { startDate: '2018-11-01T09:45', endDate: '2018-11-01T11:00', title: 'Meeting' },
+  { startDate: '2018-11-01T12:00', endDate: '2018-11-01T13:30', title: 'Go to a gym' },
+];
 
 const Schedule = () => {
     return (
-        <div>
-                 <Card>
-        <CardHeader>
-          <Checkboxes>
-            <Checkbox
-              label='Searchable'
-              checked={isSearchable}
-              onCheck={setIsSearchable}
-            />
-            <Checkbox
-              label='Clearable'
-              checked={isClearable}
-              onCheck={setIsClearable}
-            />
-            <Checkbox
-              label='Disabled'
-              checked={isDisabled}
-              onCheck={setIsDisabled}
-            />
-            <Checkbox
-              label='Invalid'
-              checked={isInvalid}
-              readOnly={isDisabled}
-              onCheck={setIsInvalid}
-            />
-            <Checkbox
-              label='Loading'
-              checked={isLoading}
-              onCheck={setIsLoading}
-            />
-          </Checkboxes>
-        </CardHeader>
-        <CardBody>
-          <SelectContainer>
-            <Select
-              isLoading={isLoading}
-              isInvalid={isInvalid}
-              options={CITY_OPTIONS}
-              isDisabled={isDisabled}
-              isClearable={isClearable}
-              isSearchable={isSearchable}
-              getOptionValue={getOptionValue}
-              getOptionLabel={getOptionLabel}
-            />
-          </SelectContainer>
-        </CardBody>
-      </Card>
+     <div>
+       <Paper>
+        <Scheduler
+         data={schedulerData}>
+         <ViewState
+         currentDate={currentDate}
+        />
+      <DayView
+        startDayHour={9}
+        endDayHour={14}
+      />
+      <Appointments />
+    </Scheduler>
+  </Paper>
         </div>
     )
 }

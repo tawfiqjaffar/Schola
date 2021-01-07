f900ec89e740f9c6b3e64f9092274bd581d35285
kevin.hu@epitech.eu <kevin.hu@epitech.eu@epitech.eu>
2021-01-07 17:10:07 +0100

    reArranging css for chat

--- packages/frontend/src/components/pages/Chatbox/Chat/Chat.js
+++ packages/frontend/src/components/pages/Chatbox/Chat/Chat.js
@@ -53,8 +53,8 @@ const Chat = ({ location }) => {
   };
 
   return (
-    <div className="outerContainer">
-      <div className="container">
+    <div className="ChatOuterContainer">
+      <div className="ChatContainer">
         <InfoBar room={room} />
         <Messages messages={messages} name={name} />
         <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />

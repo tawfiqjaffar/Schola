3c7cb51b2a220b860105f1d062c4474f220fb9fb
Tawfiq JAFFAR <tawfiq.jaffar@epitech.eu>
2021-01-13 09:59:22 +0100

    linting

--- packages/backend/notifthread.js
+++ packages/backend/notifthread.js
@@ -1,31 +1,27 @@
-/* eslint-disable no-unused-vars */
 const nodemailer = require("nodemailer");
+const Grade = require("./src/models/grade");
 const User = require("./src/models/user");
 
-const User = require('./src/models/user');
-
-const Abs = require('./src/models/absence');
-const Grade = require('./src/models/grade');
-const nodemailer = require('nodemailer');
-const mail = process.env.EMAIL
-const password = process.env.EMAIL_PWD
+const mail = process.env.EMAIL;
+const password = process.env.EMAIL_PWD;
 
 const sendMailStudentAbs = (firstName, lastName) => {
-	var transporter = nodemailer.createTransport({
-		service: 'gmail',
-		auth: {
-		  user:  mail,
-		  pass: password
-		}
-	  });
-	  var mailOptions = {
-		from: mail,
-		to: 'yassine.rachah@epitech.eu',
-		subject: "Trop d'absences : " + firstName + " " + lastName,
-		text: 'A rectifier'
-	  };
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: mail,
+      pass: password,
+    },
+  });
+  const mailOptions = {
+    from: mail,
+    to: "yassine.rachah@epitech.eu",
+    subject: `Trop d'absences : ${firstName} ${lastName}`,
+    text: "A rectifier",
+  };
 
   transporter.sendMail(mailOptions, function (error, info) {
+    console.log(info);
     if (error) {
       console.log(error);
     } else {
@@ -49,6 +45,7 @@ const sendMailStudentGrade = (firstName, lastName) => {
   };
 
   transporter.sendMail(mailOptions, function (error, info) {
+    console.log(info);
     if (error) {
       console.log(error);
     } else {
@@ -70,43 +67,6 @@ const checkGrade = (row, callback) => {
 };
 
 const checkStudent = () => {
-<<<<<<< HEAD
-	User.find({}, (err, user) => {
-		if (err) {
-			console.log('error')
-		} else {
-
-			let nb = 0;
-			user.forEach(function(row) {
-				if (row.role === "student") {
-					if (row.absence.length != 0) {
-						if (row.nextMail === row.absence.length) {
-							console.log(row.firstName)
-							sendMailStudentAbs(row.firstName, row.lastName);
-							User.findOneAndUpdate({_id: row._id}, {nextMail: row.nextMail + 2}).then(function() {
-							});
-						}
-					}
-					if (row.grade.length != 0) {
-					row.grade.forEach(function(row2) {
-						checkGrade(row2, (res) => {
-							if (res === true) {
-								nb = nb + 1
-								if (nb === row.nextMailGrade) {
-									sendMailStudentGrade(row.firstName, row.lastName);
-									User.findOneAndUpdate({_id: row._id}, {nextMailGrade: row.nextMailGrade + 2}).then(function() {
-								});
-								}
-							}
-						})
-					});
-				}
-
-				}
-			});
-		}
-	});
-=======
   User.find({}, (err, user) => {
     if (err) {
       console.log("error");
@@ -144,7 +104,6 @@ const checkStudent = () => {
       });
     }
   });
->>>>>>> 65dd52168e68dc6eb54c03a25aebdc0142282512
 };
 
 module.exports = { checkStudent };

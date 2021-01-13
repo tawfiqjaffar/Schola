7ae7b3f8e3f98e8f5cab0e298c7b30bb4921e9c6
Tawfiq JAFFAR <tawfiq.jaffar@epitech.eu>
2021-01-13 04:58:38 +0100

    linting

--- packages/backend/src/routes/classRoutes.js
+++ packages/backend/src/routes/classRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { checkFields, authenticateJwt } = require("./middleware");
+const { checkFields } = require("./middleware");
 
 const { postCreateClass } = require("../controllers/Class/classPost");
 
